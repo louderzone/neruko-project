400fa975c16f50e288aaf7bfa84026c5b2a12626
Eric Lam <ericlamyauping@gmail.com>
2020-07-22 01:53:22 +1000

    adds discord bot startup script

---
+++ Neruko/Server/Settings/DiscordSettings.cs
@@ -0,0 +1,20 @@
+﻿namespace Neruko.Server.Settings
+{
+    public class DiscordSettings
+    {
+        /// <summary>
+        /// Gets or sets the Discord bot token
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Discord app id
+        /// </summary>
+        public string AppId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Discord app secret
+        /// </summary>
+        public string AppSecret { get; set; }
+    }
+}
--- Neruko/Server/Startup.cs
+++ Neruko/Server/Startup.cs
@@ -1,8 +1,11 @@
+using System.Threading.Tasks;
+using DSharpPlus;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Neruko.Server.Settings;
 
 namespace Neruko.Server
 {
@@ -19,9 +22,26 @@ namespace Neruko.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
             services.AddRazorPages();
+            
+            _ = Task.Run(async() => await ConfigureDiscord());
+        }
+
+        /// <summary>
+        /// Configures and starts the discord runtime
+        /// </summary>
+        /// <returns></returns>
+        public async Task ConfigureDiscord()
+        {
+            var discordSettings = Configuration.GetSection("Discord")
+                .Get<DiscordSettings>();
+            var discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = discordSettings.Token,
+                TokenType = TokenType.Bot
+            });
+            await discord.ConnectAsync();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
