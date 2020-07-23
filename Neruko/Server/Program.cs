5cd303baa6c0a3c5117f5ac0639a5f910a39c52b
Eric Lam <ericlamyauping@gmail.com>
2020-07-23 17:39:23 +1000

    adds ability to auto register commands

--- Neruko/Server/Program.cs
+++ Neruko/Server/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DryIoc;
 using DryIoc.Microsoft.DependencyInjection;
 using DSharpPlus;
+using DSharpPlus.CommandsNext;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Neruko.Server.Services.Discord;
+using Neruko.Server.Services.Discord.Commands;
 using Neruko.Server.Settings;
 
 namespace Neruko.Server
@@ -36,6 +39,7 @@ namespace Neruko.Server
         {
             try
             {
+                // Get discord credentials
                 var discordSettings = configuration.GetSection("Discord")
                     .Get<DiscordSettings>();
                 if(discordSettings?.Token == null) {
@@ -44,11 +48,13 @@ namespace Neruko.Server
                                                         + " For more info, see https://github.com/louderzone/neruko-project/issues/9");
                 }
 
+                // Loads the discord client
                 var discord = new DiscordClient(new DiscordConfiguration
                 {
                     Token = discordSettings.Token,
                     TokenType = TokenType.Bot
                 });
+                ConfigureCommands(discord);
                 await discord.ConnectAsync();
                 _discordService = new DiscordService(discord);
             }
@@ -65,6 +71,37 @@ namespace Neruko.Server
             }
 
             return _discordService;
+        }
+
+        /// <summary>
+        /// Loads all `ICommandModules` in the project
+        /// </summary>
+        /// <param name="client"></param>
+        private static void ConfigureCommands(DiscordClient client)
+        {
+            var config = new CommandsNextConfiguration
+            {
+                // let's use the string prefix defined in config.json
+                StringPrefix = "/",
+                // enable responding in direct messages
+                EnableDms = false,
+                // enable mentioning the bot as a command prefix
+                EnableMentionPrefix = true
+            };
+
+            var type = typeof(ICommandModule); // Get the type of our interface
+            var modules = AppDomain.CurrentDomain.GetAssemblies() // Get the assemblies associated with our project
+                .SelectMany(s => s.GetTypes()) // Get all the types
+                .Where(p => type.IsAssignableFrom(p) // Filter to find any type that can be assigned to an ICommandModule
+                            && !p.IsInterface); // Ignore the interfaces themselves
+
+            var commands = client.UseCommandsNext(config);
+            var commandsList = modules as Type[] ?? modules.ToArray(); // Execute the Linq
+            foreach (var command in commandsList)
+            {
+                // Loop through the list and register each command module with CommandsNext
+                commands.RegisterCommands(command);
+            }
         }
 
         /// <summary>
